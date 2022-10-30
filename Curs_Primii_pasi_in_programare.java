8754e3e0f6ced8b6330ff25088b0c8464dab158d
Stefan2517 <stefiarko803@gmail.com>
2022-10-30 18:21:32 +0200

    first commit

---
+++ Curs_Primii_pasi_in_programare.java
@@ -0,0 +1,36 @@
+public class Curs_Primii_pasi_in_programare {
+    public static void main(String[] args) {
+
+/*
+    1. Noteaza in limbajul de programare java, in cadrul unui comentariu, ce
+este o variabila boolean – explica cu cuvintele tale.
+
+    Variabila de tip boolean are ca raspuns true sau false (litere mici,
+in cazul limbajului Python incepe cu majuscula).
+*/
+
+/*
+    3. Declara si initializeaza in limbajul de programare java cate o variabila
+din fiecare din urmatoarele tipuri: string, integer, float, boolean. Alege
+valorile dupa preferinta ta.
+*/
+//Declarare si initializare variabile
+        String prenume = "Stefan";
+        int varsta = 21;
+        double pret = 300.70;
+        boolean student = true;
+
+/*
+    5. Ce tipăreşte următorul segment de cod Java? Scrie segmentul de cod
+in limbajul de progrmare java si afla raspunsul.
+int i;
+for (i=1; i<20; i = i+3)
+System.out.println( i );
+ */
+        int i;
+        for (i=1; i<20; i = i+3) {
+            System.out.println( i );
+        }
+
+    }
+}
---
+++ Curs_Primii_pași_în_programare.py
@@ -0,0 +1,34 @@
+'''
+    2. Noteaza in limbajul de programare Python, in cadrul unui comentariu,
+ce este o variabila string – explica cu cuvintele tale.
+
+    Variabila de tip string reprezinta un sir de caractere delimitate de " ".
+In Python nu exista diferenta intre string si char.
+Variabilele de la string au metode ajutatoare, exe: variabila.upper() => variabila scrisa cu majuscule.
+O alta metoda: variabila.length() => arata cate caractere are variabila.
+'''
+'''
+    4. Declara si initializeaza in limbajul de programare python cate o
+variabila din fiecare din urmatoarele tipuri: string, integer, float,
+boolean. Alege valorile dupa preferinta ta.
+'''
+# Declarare si initializare variabile
+# string
+prenume = 'Stefan'
+# int
+varsta = 21
+# float
+pret = 300.70
+# boolean
+student = True
+
+'''
+int i;
+for (i=1; i<20; i = i+3)
+System.out.println( i );
+6. Scrie segmentul de cod de la exercitiul 5 in limbajul de programare
+python
+'''
+
+for i in range (1,20,3):
+    print(f'{i}')
